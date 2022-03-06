5eada62c66162ab9b1ea6cf0a125cc1b30055552
Raj Patel <rajrpatel62499@gmail.com>
2022-03-05 19:23:41 -0500

    database properties added

--- webservice/src/main/java/com/smart_quiz/webservice/app/controllers/UserController.java
+++ webservice/src/main/java/com/smart_quiz/webservice/app/controllers/UserController.java
@@ -4,13 +4,15 @@ import com.smart_quiz.webservice.constants.ApiPathConstants;
 import com.smart_quiz.webservice.constants.AppConstants;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
+@RequestMapping(value = ApiPathConstants.UserControllerPath)
 public class UserController {
 
-    @GetMapping(value = ApiPathConstants.UserControllerPath)
+    @GetMapping()
     public String helloworld() {
         return "Hello World";
     }
